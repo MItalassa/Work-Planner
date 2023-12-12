2fc9b7b507800611792677159191313fd3bb7b05
salvatore vitiello <marina.iacomino@gmail.com>
2023-12-12 17:02:53 +0000

    current day in the header added

---
+++ script.js
@@ -0,0 +1,123 @@
+//display the current date inside the paragrapf of ID currentDay
+let currentDay = $("#currentDay")
+
+let todayDate = dayjs()
+//Display the current day at the top of the calender when a user opens the planner.
+currentDay = todayDate.format('dddd DD.MM.YYYY');
+console.log(dayjs())
+
+
+
+
+
+//Present timeblocks for standard business hours when the user scrolls down.
+// function timeBlockColor() {
+//     var hour = dayjs().hour();
+//     console.log(hour)
+
+//     $(".time-block").each(function() {
+//         var currHour = parseInt($(this).attr("id"));
+
+//         // console.log(this); //each time-block
+
+//         if (currHour > hour) {
+//             $(this).addClass("future");
+//         } else if (currHour === hour) {
+//             $(this).addClass("present");
+//         } else {
+//             $(this).addClass("past");
+//         }
+//     })
+// };
+//Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+//Allow a user to enter an event when they click a timeblock
+
+//Save the event in local storage when the save button is clicked in that timeblock.
+
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+
+//Persist events between refreshes of a page
+
+var saveBtn = $(".saveBtn");
+
+/**
+ * FUNCTIONS
+ */
+
+// current day is displayed at the top of the calendar
+
+// each time block is color-coded to indicate whether it is in the past, present, or future
+
+
+// WHEN I click the save button for that time block
+saveBtn.on("click", function() {
+
+    // console.log(this); //save button
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, plan);
+});
+
+// WHEN I refresh the page
+// THEN the saved events persist
+function usePlanner() {
+
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        // console.log(this);
+        // console.log(currHour);
+
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+/**
+ * CALL FUNCTIONS
+ */
+
+timeBlockColor();
+usePlanner();
+
+// // delete single task
+// $("#trash").droppable({
+//   accept: ".card .list-group-item",
+//   tolerance: "touch",
+//   drop: function(event, ui) {
+//     ui.draggable.remove();
+//     $(".bottom-trash").removeClass("bottom-trash-active");
+//   },
+//   over: function(event, ui) {
+//     // console.log("over");
+//     $(".bottom-trash").addClass("bottom-trash-active");
+//   },
+//   out: function(event, ui) {
+//     // console.log("out");
+//     $(".bottom-trash").removeClass("bottom-trash-active");
+//   }
+// });
+           
+// // remove all tasks
+// $("#remove-tasks").on("click", function() {
+//   for (var key in tasks) {
+//     tasks[key].length = 0;
+//     $("#list-" + key).empty();
+//   }
+//   saveTasks();
+// });
+
+// // load tasks for the first time
+// loadTasks();
+
+// setInterval(function () {
+//   $(".card .list-group-item").each(function(index, el) {
+//     auditTask(el);
+//   });
+// }, (1000 * 60) * 30);
